629a743eab9e36b1b88448c563d83ee55ca759e7
hojkas <ivka.strnadova@gmail.com>
2020-05-08 20:00:28 +0200

    headers and new class

---
+++ src/icp/mapwidget.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file mapwidget.cpp
+ * @author xstrna14
+ * @brief TODO
+ * Additional description TODO
+ */
+
+#include "mapwidget.h"
+
+MapWidget::MapWidget(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ src/icp/mapwidget.h
@@ -0,0 +1,24 @@
+/**
+ * @file mapwidget.h
+ * @author xstrna14
+ * @brief TODO
+ * Additional description TODO
+ */
+
+#ifndef MAPWIDGET_H
+#define MAPWIDGET_H
+
+#include <QWidget>
+
+class MapWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MapWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // MAPWIDGET_H
