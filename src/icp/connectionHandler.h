1a82a45030e8009b3e9cc389074f0a079eb510f4
xlebod <d.lebo112@gmail.com>
2020-05-09 20:22:38 +0200

    Updated connectionHandler.h

--- src/icp/connectionHandler.h
+++ src/icp/connectionHandler.h
@@ -6,23 +6,40 @@
 #include <QJsonArray>
 #include <QJsonObject>
 #include <QString>
+#include <time.h>
+#include "street.h"
 
+class connectionElem
+{
+public:
+    QString name;
+    std::list<std::tuple<QString, bool>> streetList;
+};
+
+class busElem
+{
+    connectionElem con;
+    time_t time;
+    int x;
+    int y;
+    Street curStreet;
+};
 
 class connectionHandler : public QObject
 {
     Q_OBJECT
 public:
     explicit connectionHandler(QObject *parent = nullptr);
-
+    std::list<connectionElem> conList;
 signals:
 
 public slots:
     void loadConnections();
-
+    void printConnections();
  private:
 
 };
 
-class c
+
 
 #endif // CONNECTION_H
